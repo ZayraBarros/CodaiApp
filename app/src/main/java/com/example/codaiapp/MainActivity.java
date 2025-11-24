4c0136d749d8eb01de307e6b0d50922cf7188539
ZayraBarros <zayrita.barros@gmail.com>
2025-11-24 19:46:30 -0300

    pagina de criar conta e pagina de login

--- app/src/main/java/com/example/codaiapp/MainActivity.java
+++ app/src/main/java/com/example/codaiapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.codaiapp;
 
+import android.content.Intent; // <-- ADICIONE ISSO
 import android.os.Bundle;
+import android.widget.TextView; // <-- E ISSO
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Mantém o edge-to-edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // ===============================
+        //  AQUI você adiciona o clique!!!
+        // ===============================
+
+        TextView tvLogin = findViewById(R.id.tvLogin);
+
+        tvLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
